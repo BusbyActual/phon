f1ebe7b414760e0825c99d18fa33978e0a20cc20
gwimbrow <graham.wimbrow@gmail.com>
2015-01-29 19:49:57 -0800

    address issues with liquid and nasal consonants

--- phon.py
+++ phon.py
@@ -3,7 +3,8 @@ import sys,os,random,ast
 consonants={'p':'70','t':'74',':t':'FA','c':'63','k':'6B','q':'71','P':'50','b':'62','d':'64',':d':'E3','textbardotlessj':'E9','g':'67',';G':'E5','m':'6D','M':'4D','n':'6E',':n':'EF','textltailn':'F1','N':'4E',';N':'F0',';B':'E0','r':'72',';R':'F6','R':'52',':r':'F3','F':'46','f':'66','T':'54','s':'73','S':'53',':s':'F9',':c':'E7','x':'78','X':'58','textcrh':'E8','h':'68','B':'42','v':'76','D':'44','z':'7A','Z':'5A','J':'4A','G':'47','K':'43','Q':'51','H':'48','textbeltl':'EC','textlyoghlig':'D0','V':'56','*r':'F4',':R':'F5','j':'6A','textturnmrleg':'EE','l':'6C',':l':'ED','L':'4C',';L':'CF'}
 vowels={'i':'69','y':'79','1':'31','O':'30','W':'57','u':'75','I':'49','Y':'59','U':'55','e':'65',':o':'F8','9':'39','8':'38','7':'37','o':'6F','@':'40','E':'45','oe':'F7','3':'33','textcloseepsilon':'C5','2':'32','0':'4F','5':'35','ae':'E6','OE':'D7','a':'61','A':'41','6':'36'}
 suprasegmentals={':':'3A',';':'3B'}
-accents={'u':'08','thh':'05','th':'01','tm':'09','tl':'00','tll':'0D'}
+accents={'u':'08','thh':'05','th':'01','tm':'09','tl':'00','tll':'0D','mp':'7F'}
+morphing={'l','r','m','n','N'}
 C=[]
 V=[]
 with open(sys.argv[1]) as source: phrase,verse=ast.literal_eval(source.readline())
@@ -17,7 +18,7 @@ for p in cypher:
   else:
     V.append(p)
     rules+='v'
-#if len([rl for rl in rules.split('.') if 'vv' in rl])>0: sys.exit("ERROR: vv")
+if len([rl for rl in rules.split('.') if 'vvv' in rl or 'ccc' in rl])>0: sys.exit("ERROR: vvv or ccc")
 rhymes={}
 composition=[]
 for ln in range(len(verse)):
@@ -29,16 +30,21 @@ for ln in range(len(verse)):
     pr=''
     for d in range(len(rule)):
       if rule[d]=='c':
-        if d+1<len(rule) and rule[d+1]=='c': con=random.choice([cc[1] for cc in enumerate(cypher) if cc[0]+1<len(cypher) and cc[1] in ct and cypher[cc[0]+1] in ct])
-        else:
+        if d+1<len(rule) and rule[d+1]=='c':
+          dc=[cc[1] for cc in enumerate(cypher) if cc[0]+1<len(cypher) and cc[1] in ct and cypher[cc[0]+1] in ct]
+          if d==0: con=random.choice([pc for pc in dc if pc not in morphing])
+          else: con=random.choice(dc)
+        elif pr in C:
           try: con=random.choice([cypher[ph[0]+1] for ph in enumerate(cypher) if ph[1]==pr and ph[0]+1<len(cypher) and cypher[ph[0]+1] in ct])
-          except IndexError: con=random.choice(ct)
-        phonemes+='\ipa\char"'+consonants[con]
-        pr=con
-        for x,t in enumerate(ct):
-          if t==pr:
-            ct.pop(x)
-            break
+          except IndexError: con=''
+        else: con=random.choice(ct)
+        if con!='':
+          phonemes+='\ipa\char"'+consonants[con]
+          pr=con
+          for x,t in enumerate(ct):
+            if t==pr:
+              ct.pop(x)
+              break
       else:
         if d+1<len(rule) and rule[d+1]=='v':
           vow=random.choice([vv[1] for vv in enumerate(cypher) if vv[0]+1<len(cypher) and vv[1] in vt and cypher[vv[0]+1] in vt])
