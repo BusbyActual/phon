9543761fe663dced5747c7a62a8820c5b03ed019
gwimbrow <graham.wimbrow@gmail.com>
2015-02-15 03:58:07 -0800

    working!

--- phon.py
+++ phon.py
@@ -1,11 +1,9 @@
-# the rhyming syllable (and all matching syllables) are matching even when only part of the generated syllable exists in the source phrase. For example, "phan" would be a match for "pha" in the source.
-#
 #!/usr/bin/env python
 import sys,os,random,time
 consonants={'p':'70','t':'74',':t':'FA','c':'63','k':'6B','q':'71','P':'50','b':'62','d':'64',':d':'E3','textbardotlessj':'E9','g':'67',';G':'E5','m':'6D','M':'4D','n':'6E',':n':'EF','textltailn':'F1','N':'4E',';N':'F0',';B':'E0','r':'72',';R':'F6','R':'52',':r':'F3','F':'46','f':'66','T':'54','s':'73','S':'53',':s':'F9',':c':'E7','x':'78','X':'58','textcrh':'E8','h':'68','B':'42','v':'76','D':'44','z':'7A','Z':'5A','J':'4A','G':'47','K':'43','Q':'51','H':'48','textbeltl':'Eself.C','textlyoghlig':'D0','V':'56','*r':'F4',':R':'F5','j':'6A','textturnmrleg':'EE','l':'6C',':l':'ED','L':'4C',';L':'CF','w':'77'}
 vowels={'i':'69','y':'79','1':'31','O':'30','W':'57','u':'75','I':'49','Y':'59','U':'55','e':'65',':o':'F8','9':'39','8':'38','7':'37','o':'6F','@':'40','E':'45','oe':'F7','3':'33','textcloseepsilon':'C5','2':'32','0':'4F','5':'35','ae':'E6','OE':'D7','a':'61','A':'41','6':'36'}
 suprasegmentals={':':'3A',';':'3B','rh':'7E'}
-morphing={'l','r','m','n'} # N can only occur at end of syllable. morphing cannot occur as first of two C's unless the combination occurs in the source
+morphing={'l','r','m','n','N'}
 glides={'h','j','w'}
 class page:
   def __init__(self):
@@ -26,82 +24,77 @@ class page:
       else:
         self.V.add(c)
         self.rules+='v'
-    self.f=0
   def compose(self):
     syllables=[]
     rhymes={}
     phonemes='\\null\\vfill\n'
     for ln in range(len(self.verse)):
-      phonemes+='\centerline{'
+      phonemes+='\ifodd\pageno\else\hfill\\fi{'
       for lt in range(len(self.verse[ln])):
         if lt>0: phonemes+='\ipa\char"2E'
-        nucleus=''.join([ph for ph in self.verse[ln][lt] if ph not in self.C])
+        nucleus=''.join([p for p in self.verse[ln][lt] if p not in consonants.keys()])
         if nucleus in rhymes.keys(): rule=random.choice([pt for pt in set(self.rules.split('.')) if 'v'*len(rhymes[nucleus]) in pt])
         else: rule=random.sample(set(self.rules.split('.')),1)[0]
         pr=''
         syll=[]
+        ct=set(self.C)
+        vt=set(self.V)
         while len(syll)<len(rule):
           if rule[len(syll)]=='c':
             if len(syll)+1<len(rule) and rule[len(syll)+1]=='c':
-              pre=[ph[1] for ph in enumerate(self.cypher) if ph[0]+1<len(self.cypher) and ph[1] in self.C and self.cypher[ph[0]+1] in self.C]
+              pre=[ph[1] for ph in enumerate(self.cypher) if ph[0]+1<len(self.cypher) and ph[1] in ct and self.cypher[ph[0]+1] in ct]
               if len(syll)>0: con=random.choice([ph for ph in pre if ph not in glides])
-              else: con=random.choice(pre)
+              else: con=random.choice([ph for ph in pre if ph not in morphing])
             elif pr in self.C:
               try:
-                pre=[self.cypher[ph[0]+1] for ph in enumerate(self.cypher) if ph[1]==pr and ph[0]+1<len(self.cypher) and self.cypher[ph[0]+1] in self.C]
-                con=random.choice(pre) if len(syll)<len(rule)-1 else random.choice([ph for ph in pre if ph not in morphing])
+                pre=[self.cypher[ph[0]+1] for ph in enumerate(self.cypher) if ph[0]+1<len(self.cypher) and ph[1]==pr and self.cypher[ph[0]+1] in ct]
+                if len(syll)+1!=len(rule): con=random.choice(pre)
+                else: con=random.choice([ph for ph in pre if ph not in morphing])
               except IndexError: con=''
-            elif len(syll)!=0:
+            else:
               try:
-                pre=[ph[1] for ph in enumerate(self.cypher) if ph[1] in self.C-glides and self.cypher[ph[0]-1]==pr]
-                con=random.choice([p for p in pre if p not in syll])
-              except IndexError: con=random.choice([ph for ph in self.C if ph not in glides and ph not in syll])
-            else: con=random.choice([ph for ph in self.C if ph not in syll])
+                pre=[ph[1] for ph in enumerate(self.cypher) if ph[1] in ct-glides and self.cypher[ph[0]-1]==pr]
+                con=random.choice(pre)
+              except IndexError: con=random.sample(ct-glides,1)[0] if len(syll)>0 else random.choice([ph for ph in ct if ph!='N'])
             pr=con
+            if con in ct: ct.remove(con)
           else:
             if len(syll)+1<len(rule) and rule[len(syll)+1]=='v':
-              pre=[ph[1] for ph in enumerate(self.cypher) if ph[0]+1<len(self.cypher) and ph[1] in self.V and self.cypher[ph[0]+1] in self.V]
-              if nucleus not in rhymes.keys(): rhymes[nucleus]=[random.choice([p for p in pre if p not in syll])]
+              pre=[ph[1] for ph in enumerate(self.cypher) if ph[0]+1<len(self.cypher) and ph[1] in vt and self.cypher[ph[0]+1] in vt]
+              if nucleus not in rhymes.keys(): rhymes[nucleus]=[random.choice(pre)]
               vow=rhymes[nucleus][0]
             elif pr in self.V:
               try:
-                pre=[self.cypher[ph[0]+1] for ph in enumerate(self.cypher) if ph[1]==pr and ph[0]+1<len(self.cypher) and self.cypher[ph[0]+1] in self.V]
-                if len(rhymes[nucleus])==1: rhymes[nucleus].append(random.choice([p for p in pre if p not in syll]))
+                pre=[self.cypher[ph[0]+1] for ph in enumerate(self.cypher) if ph[1]==pr and ph[0]+1<len(self.cypher) and self.cypher[ph[0]+1] in vt]
+                if len(rhymes[nucleus])<2: rhymes[nucleus].append(random.choice(pre))
                 vow=rhymes[nucleus][1]
               except IndexError: vow=''
             else:
               if nucleus not in rhymes.keys():
-                try: rhymes[nucleus]=[random.choice([cc[1] for cc in enumerate(self.cypher) if cc[1] in self.V and self.cypher[cc[0]-1]==pr])]
-                except IndexError: rhymes[nucleus]=[random.sample(self.V,1)[0]]
+                try: rhymes[nucleus]=[random.choice([cc[1] for cc in enumerate(self.cypher) if cc[1] in vt and self.cypher[cc[0]-1]==pr])]
+                except IndexError: rhymes[nucleus]=[random.sample(vt,1)[0]]
               vow=rhymes[nucleus][0]
             pr=vow
-          if pr!='': syll.append(pr)
+            if vow in vt: vt.remove(vow)
+          syll.append(pr)
         sb=''.join(syll)
         syllables.append(sb)
         if sb in self.sylbs:
           phonemes+='\pdfcolorstack\match push{1 0 0 rg}'
           self.matching.add(sb)
-        for s in range(len(syll)):
-          if '/' not in syll[s]: phonemes+='\ipa\char"'+dict(consonants.items()+vowels.items())[syll[s]]
+        for s in [s for s in syll if s!='']:
+          if '/' not in s: phonemes+='\ipa\char"'+dict(consonants.items()+vowels.items())[s]
           else:
-            va=syll[s].split('/')
+            va=s.split('/')
             if va[1] in suprasegmentals.keys(): phonemes+='\ipa\char"'+vowels[va[0]]+'\ipa\char"'+suprasegmentals[va[1]]
         if sb in self.sylbs: phonemes+='\pdfcolorstack\match pop{}'
       if ln<len(self.verse)-1: phonemes+='}\\bigskip\n'
-    if len([s for s in syllables if s in self.sylbs])>0:
-      print('page '+str(self.f))
-      self.f+=1
-      with open('log','a') as log: log.write(' '.join([s for s in syllables if s in self.sylbs])+'\n')
-      return phonemes+'}\n\\vfill\eject\n'
+    if len([s for s in syllables if s in self.sylbs])>0: return phonemes+'}\n\\vfill\eject\n'
     else: return ''
 pg=page()
-def main():
-  with open('log','w') as log: log.write('matching syllables:\n')
-  temp=open(sys.argv[1]+'.tex','w')
-  temp.write('\\footline={\\tenrm\it '+time.strftime("%d.%m.%y")+'\hfill\\folio\hfill'+time.strftime("%H.%M.%S")+'}\chardef\match=\pdfcolorstackinit page direct{0 g} \\font\ipa=tipa17 \pdfpagewidth 5.5in \pdfpageheight 8.5in \hsize 4.5in \\vsize 7in \hoffset -0.5in \\voffset -0.375in\n')
-  while pg.matching!=pg.sylbs:
-    temp.write(pg.compose())
-  temp.write('\\bye')
-  temp.close()
-  os.system('pdftex '+sys.argv[1]+'.tex')
-main()
+temp=open(sys.argv[1]+'.tex','w')
+temp.write('\chardef\match=\pdfcolorstackinit page direct{0 g} \\font\ipa=tipa17 \\font\\title=cmr17 \parindent 0pt \pdfpagewidth 5truein \pdfpageheight 7truein \pdfhorigin 1truein \pdfvorigin 1truein \hsize 3.5truein \\vsize 5truein \hoffset -0.25truein \\voffset -0.5truein \pdfcompresslevel=0 \n\\null\\bigskip\\title\pdfcolorstack\match push{1 0 0 rg}CIPHER\pdfcolorstack\match pop{} '+time.strftime("%m.%y")+'\\nopagenumbers\\vfill\eject\n\\null\supereject\n\pageno=1\\footline={\\tenrm\ifodd\pageno\hfill\\folio\else\\folio\hfill\\fi}')
+while pg.matching!=pg.sylbs: temp.write(pg.compose())
+temp.write('\\bye')
+temp.close()
+os.system('pdftex '+sys.argv[1]+'.tex')
