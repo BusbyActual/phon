051c8d7815412792c5b0532ce9d20c8897b40e50
gwimbrow <graham.wimbrow@gmail.com>
2015-03-08 15:21:45 -0700

    updated pagination to produce 4-page folios

--- phon.py
+++ phon.py
@@ -6,16 +6,15 @@ suprasegmentals={':':'3A',';':'3B','rh':'7E'}
 morphing={'l','r','m','n','N'}
 glides={'h','j','w'}
 with open(sys.argv[1]) as source: lines=map(lambda x: x.rstrip(), source.readlines())
-cypher=' . '.join(lines).split(' ')
-sylbs=''.join(cypher).split('.')
+cipher=' . '.join(lines).split(' ')
+sylbs=''.join(cipher).split('.')
 verse=map(lambda x: ''.join([y for y in x if y!=' ']).split('.'), lines)
 rules=''
 C=set()
 V=set()
-matching=[]
-switch=[0,-1]
-lit=['\pdfcompresslevel=0\chardef\match=\pdfcolorstackinit page direct{0 g}\\nopagenumbers\\font\ipa=tipa12\\font\\title=cmr17\pdfpagewidth 210mm\pdfpageheight 148mm\pdfhorigin 12mm\pdfvorigin 16mm\hsize 162mm\\vsize 100mm\\null\\vfill\eject\n']
-for c in cypher:
+lit=[]
+match=False
+for c in cipher:
   if c in consonants.keys():
     C.add(c)
     rules+='c'
@@ -23,11 +22,12 @@ for c in cypher:
   else:
     V.add(c)
     rules+='v'
-m=len(sylbs)
+
+# build only lists of phonemes, and add the page handlers later.
+
 while True:
   rhymes={}
-  phonemes='\hbox to 186mm{\hsize=81mm' if len(lit)%2!=0 else ''
-  phonemes+='\\vbox to 100mm{\\vfill'
+  phonemes=''
   for ln in range(len(verse)):
     for lt in range(len(verse[ln])):
       if lt>0: phonemes+='\ipa\char"2E'
@@ -41,61 +41,65 @@ while True:
       while len(syll)<len(rule):
         if rule[len(syll)]=='c':
           if len(syll)+1<len(rule) and rule[len(syll)+1]=='c':
-            if 'ccc' in rule and len(syll)==0:
-              con=random.choice([ph[1] for ph in enumerate(cypher) if ph[0]+2<len(cypher) and cypher[ph[0]+1] in ct and cypher[ph[0]+2]=='r'])
+            if 'ccc' in rule and len(syll)==0: con=random.choice([ph[1] for ph in enumerate(cipher) if ph[0]+2<len(cipher) and cipher[ph[0]+1] in ct and cipher[ph[0]+2]=='r'])
             elif pr in C:
-              try: con=random.choice([ph[1] for ph in enumerate(cypher) if ph[0]+1<len(cypher) and ph[1] in ct and ph[1]!=pr and cypher[ph[0]+1]=='r'])
+              try: con=random.choice([ph[1] for ph in enumerate(cipher) if ph[0]+1<len(cipher) and ph[1] in ct and ph[1]!=pr and cipher[ph[0]+1]=='r'])
               except IndexError: con=''
             else:
-              pre=[ph[1] for ph in enumerate(cypher) if ph[0]+1<len(cypher) and ph[1] in ct and ph[1]!=pr and cypher[ph[0]+1] in ct]
+              pre=[ph[1] for ph in enumerate(cipher) if ph[0]+1<len(cipher) and ph[1] in ct and ph[1]!=pr and cipher[ph[0]+1] in ct]
               if len(syll)>0: con=random.choice([ph for ph in pre if ph not in glides and ph!=pr])
               else: con=random.choice([ph for ph in pre if ph not in morphing and ph!=pr])
           elif pr in C:
             try:
-              pre=[cypher[ph[0]+1] for ph in enumerate(cypher) if ph[0]+1<len(cypher) and ph[1]==pr and cypher[ph[0]+1] in ct and cypher[ph[0]+1]!=pr]
+              pre=[cipher[ph[0]+1] for ph in enumerate(cipher) if ph[0]+1<len(cipher) and ph[1]==pr and cipher[ph[0]+1] in ct and cipher[ph[0]+1]!=pr]
               if len(syll)+1!=len(rule): con=random.choice(pre)
               else: con=random.choice([ph for ph in pre if ph not in morphing and ph!=pr])
             except IndexError: con=''
           else:
             try:
-              pre=[ph[1] for ph in enumerate(cypher) if ph[1] in ct-glides and ph[1]!=pr and cypher[ph[0]-1]==pr]
+              pre=[ph[1] for ph in enumerate(cipher) if ph[1] in ct-glides and ph[1]!=pr and cipher[ph[0]-1]==pr]
               con=random.choice(pre)
             except IndexError: con=random.choice([ph for ph in ct if ph not in glides and ph!=pr]) if len(syll)>0 else random.choice([ph for ph in ct if ph!='N' and ph!=pr])
           pr=con
         else:
           if len(syll)+1<len(rule) and rule[len(syll)+1]=='v':
-            pre=[ph[1] for ph in enumerate(cypher) if ph[0]+1<len(cypher) and ph[1] in vt and cypher[ph[0]+1] in vt]
+            pre=[ph[1] for ph in enumerate(cipher) if ph[0]+1<len(cipher) and ph[1] in vt and cipher[ph[0]+1] in vt]
             if nucleus not in rhymes.keys(): rhymes[nucleus]=[random.choice(pre)]
             vow=rhymes[nucleus][0]
           elif pr in V:
             try:
-              pre=[cypher[ph[0]+1] for ph in enumerate(cypher) if ph[1]==pr and ph[0]+1<len(cypher) and cypher[ph[0]+1] in vt]
+              pre=[cipher[ph[0]+1] for ph in enumerate(cipher) if ph[1]==pr and ph[0]+1<len(cipher) and cipher[ph[0]+1] in vt]
               if len(rhymes[nucleus])<2: rhymes[nucleus].append(random.choice(pre))
               vow=rhymes[nucleus][1]
             except IndexError: vow=''
           else:
             if nucleus not in rhymes.keys():
-              try: rhymes[nucleus]=[random.choice([ph[1] for ph in enumerate(cypher) if ph[1] in vt and cypher[ph[0]-1]==pr])]
+              try: rhymes[nucleus]=[random.choice([ph[1] for ph in enumerate(cipher) if ph[1] in vt and cipher[ph[0]-1]==pr])]
               except IndexError: rhymes[nucleus]=[random.sample(vt,1)[0]]
             vow=rhymes[nucleus][0]
           pr=vow
         syll.append(pr)
       sb=''.join(syll)
-      if len(sylbs)>0 and sb==sylbs[switch[0]]: phonemes+='\pdfcolorstack\match push{1 0 0 rg}'
+      if len(sylbs)>0 and sb==sylbs[0] and match!=True: phonemes+='\pdfcolorstack\match push{1 0 0 rg}'
       for s in [s for s in syll if s!='']:
         if '/' not in s: phonemes+='\ipa\char"'+dict(consonants.items()+vowels.items())[s]
         elif s.split('/')[1] in suprasegmentals.keys(): phonemes+='\ipa\char"'+vowels[s.split('/')[0]]+'\ipa\char"'+suprasegmentals[s.split('/')[1]]
-      if len(sylbs)>0 and sb==sylbs[switch[0]]:
+      if len(sylbs)>0 and sb==sylbs[0] and match!=True:
         phonemes+='\pdfcolorstack\match pop{}'
-        sylbs.pop(switch[0])
+        sylbs.pop(0)
+        match=True
     if ln<len(verse)-1: phonemes+='\medskip'
-  if len(sylbs)<m:
-    m=len(sylbs)
-    if len(lit)%2!=0:
-      lit.append(phonemes+'\\vfill}\hfill')
-      switch=switch[::-1]
-    else: lit.append(phonemes+'\\vfill}}\eject\n')
+  if match==True:
+    match=False
+    lit.append(phonemes)
   elif len(sylbs)==0: break
-if len(lit)%2==0: lit.append('}\eject\n')
-with open(sys.argv[1]+'.tex','w') as temp: temp.write(''.join(lit)+'\\bye')
-os.system('pdftex '+sys.argv[1]+'.tex')
+temp=open(sys.argv[1]+'.tex','w')
+temp.write('\pdfcompresslevel=0\chardef\match=\pdfcolorstackinit page direct{0 g}\\nopagenumbers\\font\ipa=tipa12\\font\\title=cmr17\pdfpagewidth 210mm\pdfpageheight 148mm\pdfhorigin 12mm\pdfvorigin 16mm\hsize 162mm\\vsize 100mm\n')
+script=[lit[x:x+4] for x in range(0,len(lit),4)]
+for l in range(len(script)):
+  script[l].insert(0,script[l].pop(-1))
+  for m in range(0,len(script[l]),2):
+    temp.write('\hbox to 186mm{\hsize=81mm\\vbox to 100mm{\\vfill'+script[l][m]+'\\vfill}\hfill\\vbox to 100mm{\\vfill'+script[l][m+1]+'\\vfill}}')
+    if (m*2)*(l+1)!=len(script)*4: temp.write('\eject\n')
+    else: temp.write('\\bye')
+temp.close
